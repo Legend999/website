ab7aab493f646c1569291081efad4a23a6d30c22
Damian D <demolka.02@wp.pl>
2021-05-19 20:00:54 +0200

    inicjacja projektu

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="pl-PL">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Gotu|Permanent Marker" rel="stylesheet">
+    </head>
+    <body>
+    </body>
+</html>
