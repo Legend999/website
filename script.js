83ffd46ac9ae8b00052d2e243119429e5d27aa79
Legend999 <demolka.02@wp.pl>
2021-05-20 12:30:57 +0200

    v.0.02

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ $(document).ready(function () {
     $.get("dictionary.umg", function (response) {
         var file = response;
         var splitted = file.split("\n"); //splitted into an array of substring
-        console.log(splitted);
+        for (var i = 0; i < splitted.length; ++i) {
+            if (splitted[i] == "")
+                break;
+            var tmp = splitted[i].split("-");
+            tmp.push.apply(tmp, [i, 0]); // [id, value]
+            
+            $('<div class="row ROW" id="u'+(i+1)+'"><div class="col pol">'+tmp[0]+'</div><div class="col eng">'+tmp[1]+'</div></div>').insertAfter("#u"+i);
+        }
     });
 
     $('#search').keyup(function () {
