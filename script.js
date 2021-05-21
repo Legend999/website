fcdf29a907fbb41580ef38fcfbe060e154a4d620
Legend999 <demolka.02@wp.pl>
2021-05-21 21:33:38 +0200

    v0.06

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const options = {
     // findAllMatches: false,
     // minMatchCharLength: 1, //mozna ustawic na 2
     // location: 0,
-    // threshold: 0.6,
+    threshold: 0.3,
     // distance: 100,
     // useExtendedSearch: false,
     ignoreLocation: true,
@@ -34,13 +34,21 @@ $(document).ready(function () {
 
             $('<div class="row ROW u' + (i + 1) + '"><div class="col pol">' + tmp[0] + '</div><div class="col eng">' + tmp[1] + '</div></div>').insertAfter(".u" + i);
         }
-        console.log(list);
+        $('<div class="u' + (i + 1) + '"></div>').insertAfter(".u" + i);
+        let tmp2 = {
+            "pl": "",
+            "en": "",
+            "id": i,
+            "value": i
+        };
+        list.push(tmp2);
     });
 
-    var typingTimer;
-    var doneTypingInterval = 1000;
+    var typingTimer, num = 0;
+    var doneTypingInterval = 200;
     $('#search').keydown(function () {
         clearTimeout(typingTimer);
+        ++num;
     });
     $('#search').keyup(function () {
         clearTimeout(typingTimer);
@@ -48,17 +56,32 @@ $(document).ready(function () {
     });
 
     async function doneTyping() {
+        let tmp_num = num;
         var pattern = $('#search').val();
         const fuse = new Fuse(list, options);
         var result = fuse.search(pattern);
 
-        for (var i = 0; i < Math.min(10,result.length); ++i) {
+        for (var i = 0;
+            (i < Math.min(4, result.length)) && (tmp_num == num); ++i) {
             let j;
             for (j = 0; list[j]['id'] != result[i]['item']['id']; ++j);
-            while (j > i) {
-                $(".u" + (list[j]['value'])).addClass("go-down");
-                $(".u" + (list[j + 1]['value'])).addClass("go-up");
-                await new Promise(r => setTimeout(r, 150));
+            while ((j > 10) && (tmp_num == num)) {
+                $(".u" + list[j]['value']).addClass("go-down-fast");
+                $(".u" + (list[j]['value'] + 1)).addClass("go-up-fast");
+                await new Promise(r => setTimeout(r, 2));
+                --j;
+                var tmp = $(".u" + list[j + 2]['value']).html();
+                $(".u" + list[j + 2]['value']).html($(".u" + list[j + 1]['value']).html());
+                $(".u" + list[j + 1]['value']).html(tmp);
+                $(".u" + list[j + 1]['value']).removeClass("go-down-fast");
+                $(".u" + list[j + 2]['value']).removeClass("go-up-fast");
+                [list[j], list[j + 1]] = [list[j + 1], list[j]];
+                [list[j]['value'], list[j + 1]['value']] = [list[j + 1]['value'], list[j]['value']];
+            }
+            while ((j > i) && (tmp_num == num)) {
+                $(".u" + list[j]['value']).addClass("go-down");
+                $(".u" + (list[j]['value'] + 1)).addClass("go-up");
+                await new Promise(r => setTimeout(r, 40));
                 --j;
                 var tmp = $(".u" + list[j + 2]['value']).html();
                 $(".u" + list[j + 2]['value']).html($(".u" + list[j + 1]['value']).html());
@@ -67,8 +90,8 @@ $(document).ready(function () {
                 $(".u" + list[j + 2]['value']).removeClass("go-up");
                 [list[j], list[j + 1]] = [list[j + 1], list[j]];
                 [list[j]['value'], list[j + 1]['value']] = [list[j + 1]['value'], list[j]['value']];
-                console.log(list);
             }
         }
+        console.log(list);
     }
 });
