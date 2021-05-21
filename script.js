a027ff582ef23eb9f3c30b659bbdf2b506b205ad
Legend999 <demolka.02@wp.pl>
2021-05-21 18:34:32 +0200

    v.0.04 fuzzy search prototype

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
+const options = {
+    // isCaseSensitive: false,
+    includeScore: true,
+    // shouldSort: true,
+    // includeMatches: false, //interesting option
+    // findAllMatches: false,
+    // minMatchCharLength: 1, //mozna ustawic na 2
+    // location: 0,
+    // threshold: 0.6,
+    // distance: 100,
+    // useExtendedSearch: false,
+    ignoreLocation: true,
+    // ignoreFieldNorm: false,
+    keys: ['pl', 'en']
+};
+
 $(document).ready(function () {
     $("#search").focus();
-
+    var list = [];
     $.get("dictionary.umg", function (response) {
         var file = response;
         var splitted = file.split("\n"); //splitted into an array of substring
         for (var i = 0; i < splitted.length; ++i) {
             if (splitted[i] == "")
                 break;
-            var tmp = splitted[i].split("-");
-            tmp.push.apply(tmp, [i, 0]); // [id, value]
+            var tmp = splitted[i].split("=");
+            let tmp2 = {
+                "pl": tmp[0],
+                "en": tmp[1],
+                "id": i,
+                "value": i
+            };
+            list.push(tmp2);
 
             $('<div class="row ROW u' + (i + 1) + '"><div class="col pol">' + tmp[0] + '</div><div class="col eng">' + tmp[1] + '</div></div>').insertAfter(".u" + i);
         }
+        console.log(list);
     });
 
-
     var typingTimer;
-    var doneTypingInterval = 1000;  
+    var doneTypingInterval = 1000;
     $('#search').keydown(function () {
         clearTimeout(typingTimer);
     });
@@ -24,21 +46,34 @@ $(document).ready(function () {
         clearTimeout(typingTimer);
         typingTimer = setTimeout(doneTyping, doneTypingInterval);
     });
-    
-    function doneTyping () {
-        console.log($('#search').val());
-        $(".u2").addClass("go-down");
-        $(".u3").addClass("go-up");
 
-        $(".go-down").addClass("u3").removeClass("u2")
-        $(".go-up").addClass("u2").removeClass("u3");
+    function doneTyping() {
+        var pattern = $('#search').val();
+        const fuse = new Fuse(list, options);
+        var result = fuse.search(pattern);
 
-        setTimeout(function () {
-            var tmp = $(".u3").html();
-            $(".u3").html($(".u2").html());
-            $(".u2").html(tmp);
-            $(".u3").removeClass("go-down");
-            $(".u2").removeClass("go-up");
-        }, 500);
-    } 
+        for (var i = 0; i < 1; ++i) {
+            let j;
+            for (j = 0; list[j]['id'] != result[i]['item']['id']; ++j);
+            var FOR = window.setInterval(function () {
+                if (j == 0) {
+                    clearInterval(FOR);
+                    return;
+                }
+                $(".u" + (list[j]['value'])).addClass("go-down");
+                $(".u" + (list[j + 1]['value'])).addClass("go-up");
+                setTimeout(function () {
+                    var tmp = $(".u" + list[j + 2]['value']).html();
+                    $(".u" + list[j + 2]['value']).html($(".u" + list[j + 1]['value']).html());
+                    $(".u" + list[j + 1]['value']).html(tmp);
+                    $(".u" + list[j + 1]['value']).removeClass("go-down");
+                    $(".u" + list[j + 2]['value']).removeClass("go-up");
+                    [list[j], list[j + 1]] = [list[j + 1], list[j]];
+                    [list[j]['value'], list[j + 1]['value']] = [list[j + 1]['value'], list[j]['value']];
+                    console.log(list);
+                }, 300);
+                --j;
+            }, 310)
+        }
+    }
 });
